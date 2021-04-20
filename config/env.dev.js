2a2968e9e7c5faa589773c7a78d43f79a41d250e
Eva Olausson <eva.olausson@chasacademy.se>
2021-04-20 21:15:59 +0200

    Added config

--- config/env.dev.js
+++ config/env.dev.js
@@ -1,7 +1,9 @@
 const serverPort = process.env.PORT || 5000;
 const clientOrigins = ["http://localhost:3000", "https://u11app.herokuapp.com"];
+const clientOriginUrl = process.env.CLIENT_ORIGIN_URL;
 
 module.exports = {
   serverPort,
   clientOrigins,
+  clientOriginUrl,
 };
